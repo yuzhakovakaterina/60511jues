4101ef4002ba7fb0881e897d33497960840d0fa6
katerina <yuzhakova_es@edu.surgu.ru>
2025-02-15 22:06:23 +0500

    аше сообщение коммита

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>605-11</title>
+</head>
+<body>
+    <h2>{{ $title }}</h2>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return view('hello', ['title' => 'Hello World!']);
+});
